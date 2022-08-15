6c283381eea23e0c79ed0209b2cbe4b358e2e6f7
SofiaDrejmark <92430776+SofiaDrejmark@users.noreply.github.com>
2022-08-15 13:42:10 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const knex = require("./db");
 const io = new Server(server, {
   cors: {
     origin: ["https://admin.socket.io", "http://localhost:3000", "https://amritb.github.io/", "https://sofia-waitinglist.herokuapp.com/"],
-    credentials: true
+    
   },
 });
 
